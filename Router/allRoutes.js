8c56c9371d08e63bf6e577229007b6391545a452
Thomas Ropert <thomas.ropert@yahoo.fr>
2025-03-27 22:35:13 +0100

    Ajout système routage

---
+++ Router/allRoutes.js
@@ -0,0 +1,10 @@
+import Route from "./route.js";
+//Définir ici vos routes
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),];
+    
+
+
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Gîte bleu";
